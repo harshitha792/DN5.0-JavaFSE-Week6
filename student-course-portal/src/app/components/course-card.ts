fc2a6b204c0a75409a1c1e9a5973f939a85266f7
harshitha792 <YOUR_GITHUB_EMAIL>
2026-07-25 22:30:41 -0700

    Completed-Angular-Hands-on-2

---
+++ student-course-portal/src/app/components/course-card.ts
@@ -0,0 +1,30 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css'
+})
+export class CourseCard implements OnChanges {
+  @Input() course!: {
+    id: number;
+    name: string;
+    code: string;
+    credits: number;
+  };
+
+  @Output() enrollRequested = new EventEmitter<number>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Previous:', changes['course']?.previousValue);
+    console.log('Current:', changes['course']?.currentValue);
+  }
+}
--- student-course-portal/src/app/pages/course-list/course-list.ts
+++ student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
-  styleUrl: './course-list.css',
+  styleUrl: './course-list.css'
 })
 export class CourseList {
+  selectedCourseId?: number;
 
+  courses = [
+    { id: 1, name: 'Java', code: 'CS101', credits: 4 },
+    { id: 2, name: 'Angular', code: 'CS102', credits: 4 },
+    { id: 3, name: 'SQL', code: 'CS103', credits: 3 },
+    { id: 4, name: 'Spring Boot', code: 'CS104', credits: 4 },
+    { id: 5, name: 'Git', code: 'CS105', credits: 2 }
+  ];
+
+  onEnroll(courseId: number): void {
+    this.selectedCourseId = courseId;
+  }
 }
